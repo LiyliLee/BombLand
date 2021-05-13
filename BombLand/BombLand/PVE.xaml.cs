c5176f9d8e084be915fda333479e203300ffba9e
liyliLee <liyli@ucm.es>
2021-05-13 15:22:51 +0200

    Terminado PVE

--- BombLand/BombLand/PVE.xaml.cs
+++ BombLand/BombLand/PVE.xaml.cs
@@ -26,5 +26,10 @@ namespace BombLand
         {
             this.InitializeComponent();
         }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
