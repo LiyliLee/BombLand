8e9571c4a3132c52f8af966e8ddf2a0d454eaf2e
pablos16 <pablos16@ucm.es>
2021-05-18 13:58:37 +0200

    hecho la pantalla de pvp

--- BombLand/BombLand/PVP.xaml.cs
+++ BombLand/BombLand/PVP.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Gaming.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +23,80 @@ namespace BombLand
     /// </summary>
     public sealed partial class PVP : Page
     {
+        private readonly object myLock = new object();
+        private List<Gamepad> myGamepads = new List<Gamepad>();
+        private Gamepad mainGamepad = null;
+
         public PVP()
         {
             this.InitializeComponent();
+
+            Gamepad.GamepadAdded += (object sender, Gamepad e) => {
+                // Check if the just-added gamepad is already in myGamepads; if it isn't, add
+                // it.
+                lock (myLock) {
+                    bool gamepadInList = myGamepads.Contains(e);
+                    if (!gamepadInList) {
+                        myGamepads.Add(e);
+                    }
+                }
+            };
+
+            Gamepad.GamepadRemoved += (object sender, Gamepad e) => {
+                lock (myLock) {
+                    int indexRemoved = myGamepads.IndexOf(e);
+                    if (indexRemoved > -1) {
+                        if (mainGamepad == myGamepads[indexRemoved]) {
+                            mainGamepad = null;
+                        }
+                        myGamepads.RemoveAt(indexRemoved);
+                    }
+                }
+            };
+        }
+
+        private void PVPVolver_Click(object sender, RoutedEventArgs e) {
+            this.Frame.Navigate(typeof(MenuPrincipal));
+        }
+
+        private void botonJugar_Click(object sender, RoutedEventArgs e) {
+            this.Frame.Navigate(typeof(Juego));
+        }
+
+        private void openInviteLeft_Click(object sender, RoutedEventArgs e) {
+            openInviteLeft.Visibility = Visibility.Collapsed;
+            IOLB.Visibility = Visibility.Visible;
+            IOLBButton.Visibility = Visibility.Visible;
+        }
+
+        private void IOLBButton_Click(object sender, RoutedEventArgs e) {
+            openInviteLeft.Visibility = Visibility.Visible;
+            IOLB.Visibility = Visibility.Collapsed;
+            IOLBButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void openInviteLeftCenter_Click(object sender, RoutedEventArgs e) {
+            openInviteLeftCenter.Visibility = Visibility.Collapsed;
+            IOLC.Visibility = Visibility.Visible;
+            IOLCButton.Visibility = Visibility.Visible;
+        }
+
+        private void IOLCButton_Click(object sender, RoutedEventArgs e) {
+            openInviteLeftCenter.Visibility = Visibility.Visible;
+            IOLC.Visibility = Visibility.Collapsed;
+            IOLCButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void openInviteRight_Click(object sender, RoutedEventArgs e) {
+            openInviteRight.Visibility = Visibility.Collapsed;
+            IORB.Visibility = Visibility.Visible;
+            IORBButton.Visibility = Visibility.Visible;
+        }
+
+        private void IORBButton_Click(object sender, RoutedEventArgs e) {
+            openInviteRight.Visibility = Visibility.Visible;
+            IORB.Visibility = Visibility.Collapsed;
+            IORBButton.Visibility = Visibility.Collapsed;
         }
     }
 }
